e97a2f53443f0f31a9822c25d67a341176d4cd66
Charlike Mike Reagent <mamemto_100@mail.bg>
2014-12-21 22:26:15 +0200

    add support for app arguments

--- test.js
+++ test.js
@@ -1,29 +1,40 @@
 'use strict';
 var assert = require('assert');
-var opn = require('./');
+var opn = require('./index');
 
 // tests only checks that opening doesn't return an error
 // it has no way make sure that it actually opened anything
 
 // these have to be manually verified
 
-it('should open file in default app', function () {
-	opn('index.js');
-});
-
-it('should open url in default app', function (cb) {
-	this.timeout(20000);
-
-	opn('http://sindresorhus.com', process.platform === 'darwin' ? cb() : function (err) {
-		assert(!err, err);
+describe('opn:', function () {
+	it('should open file in default app', function (cb) {
+		opn('index.js');
 		cb();
 	});
-});
 
-it('should open url in specified app', function (cb) {
-	this.timeout(20000);
-	opn('http://sindresorhus.com', 'firefox', function (err) {
-		assert(!err, err);
-		cb();
+	it('should open url in default app', function (cb) {
+		this.timeout(20000);
+
+		opn('http://sindresorhus.com', process.platform === 'darwin' ? cb() : function (err) {
+			assert.strictEqual(!err, true);
+			cb();
+		});
+	});
+
+	it('should open url in specified app', function (cb) {
+		this.timeout(20000);
+		opn('https://twitter.com/sindresorhus', 'google-chrome', function (err) {
+			assert.strictEqual(!err, true);
+			cb();
+		});
+	});
+
+	it('should open url in specified app with arguments', function (cb) {
+		this.timeout(20000);
+		opn('https://github.com/regexps', ['google-chrome', '--incognito'], function (err) {
+			assert.strictEqual(!err, true);
+			cb();
+		});
 	});
 });
