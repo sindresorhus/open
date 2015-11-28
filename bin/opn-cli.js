98f4a97cf1f033febd161904e15bc044855e6876
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-11-28 18:27:54 +0100

    detect and try to fix malformed url

--- bin/opn-cli.js
+++ bin/opn-cli.js
@@ -2,16 +2,28 @@
 
 var opn = require('../index.js');
 var args = process.argv.slice(2);
+var url = require('url');
+var uri;
 
 if(!args[0])
-	quit("opn need a URL to open");
-	
-if(args[1])
-	opn(args[0], { app: args.slice(1) });
-else
-	opn(args[0]);
+	quit('opn need a URL to open');
 
-function quit(msg) {
-	console.error(msg);
+uri = url.parse(args[0])
+
+if(!uri.protocol)
+	uri = url.parse('http://' + args[0]);
+
+if(!uri.hostname)
+	quit('opn can not open an invalid URL: "', args[0] + '"');
+
+uri = url.format(uri);
+
+if(args[1])
+	opn(uri, { app: args.slice(1) });
+else
+	opn(uri);
+
+function quit(/* arguments */) {
+	console.error(([].slice.apply(arguments)).join(''));
 	process.exit(1)
 }
