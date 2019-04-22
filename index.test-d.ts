38d6d9f917612865f873af5b5dfaa2a2ff118d17
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-22 17:26:55 +0200

    Add TypeScript definition

---
+++ index.test-d.ts
@@ -0,0 +1,11 @@
+import {expectType} from 'tsd';
+import {ChildProcess} from 'child_process';
+import open = require('.');
+
+const options: open.Options = {};
+
+expectType<Promise<ChildProcess>>(open('foo'));
+expectType<Promise<ChildProcess>>(open('foo', {app: 'bar'}));
+expectType<Promise<ChildProcess>>(open('foo', {app: ['bar', '--arg']}));
+expectType<Promise<ChildProcess>>(open('foo', {wait: true}));
+expectType<Promise<ChildProcess>>(open('foo', {background: true}));
