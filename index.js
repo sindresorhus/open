dd3ebe86ed9b933f9b3992e3627626248d2aefee
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-11-12 11:40:31 +0100

    Handle undefined import.meta.url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import defaultBrowser from 'default-browser';
 import isInsideContainer from 'is-inside-container';
 
 // Path to included `xdg-open`.
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url || process.execPath));
 const localXdgOpenPath = path.join(__dirname, 'xdg-open');
 
 const {platform, arch} = process;
